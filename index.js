cffbd293c9beaf5b24e3ba3039ae5864fac532d6
tamotsu ichiba <ichibat@gmail.com>
2019-12-22 17:20:33 +0900

    before App.js modification

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+// IMPORT MODELS
+require('./models/Product');
+
 const app = express();
 
 mongoose.Promise = global.Promise;
@@ -17,6 +20,19 @@ mongoose.connect("mongodb://tims:Hit135Run@ds019946.mlab.com:19946/ktmethod", {
   });
 
 app.use(bodyParser.json());
+
+//IMPORT ROUTES
+require('./routes/productRoute')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
